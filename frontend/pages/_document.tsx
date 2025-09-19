9c84632f201ac52c8b89607d30e913864bba5004
Julia Nordqvist <juliaanordqvist@hotmail.com>
2025-09-19 16:43:33 +0200

    login-page first layout
    Also some changes in the global.css and _document.tsx for the font family and different devices.

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* this makes the website responsive across mobile and tablet devices*/}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
