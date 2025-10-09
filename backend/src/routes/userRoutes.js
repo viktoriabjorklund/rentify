2af93908a881fe893b06d8fa0af5614ea6682987
Kristin Eriksson <erikristin01@gmail.com>
2025-10-09 14:32:42 +0200

    added authMiddleware to the routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import * as userController from '../controllers/userController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', userController.register);
 router.put('/:id', authMiddleware, userController.updateUser);
-router.get('/:id', authMiddleware, userController.displayUser)
+router.get('/:id', authMiddleware, userController.displayUser);
 router.delete('/:id', authMiddleware, userController.deleteUser);
 router.post('/login', userController.login);
 
