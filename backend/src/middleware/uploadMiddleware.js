6a9faa38afd9c62f2b3f67f216e04ad4d23adc05
Viktoria Björklund <viktoriabjorklund99@gmail.com>
2025-10-10 15:07:51 +0200

    change file size

--- backend/src/middleware/uploadMiddleware.js
+++ backend/src/middleware/uploadMiddleware.js
@@ -45,5 +45,5 @@ const fileFilter = (req, file, cb) => {
 export const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 20 * 1024 * 1024 },
 });
